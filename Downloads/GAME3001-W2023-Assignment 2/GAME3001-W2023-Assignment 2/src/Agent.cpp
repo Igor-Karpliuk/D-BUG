e554b8447db971c5cf68867dd2d07520ef02d8ef
Devin <devinsgillman@gmail.com>
2023-02-28 18:51:35 -0500

    Added Start scene and added keys to reset and find shortest path

---
+++ Downloads/GAME3001-W2023-Assignment 2/GAME3001-W2023-Assignment 2/src/Agent.cpp
@@ -0,0 +1,200 @@
+#include "Agent.h"
+#include "Util.h"
+
+Agent::Agent()
+{
+	// initialize whisker colour
+	m_lineColour[0] = glm::vec4(0, 0, 1, 1); // left line colour 
+	m_lineColour[1] = glm::vec4(0, 1, 0, 1); // middle line colour 
+	m_lineColour[2] = glm::vec4(0, 0, 1, 1); // right line colour 
+	m_lineColour[3] = glm::vec4(0, 1, 0, 1); // far left line colour
+	m_lineColour[4] = glm::vec4(0, 1, 0, 1); // far right line colour
+
+	// initialize whisker collision booleans
+	m_collisionWhiskers[0] = false;
+	m_collisionWhiskers[1] = false;
+	m_collisionWhiskers[2] = false;
+	m_collisionWhiskers[3] = false;
+	m_collisionWhiskers[4] = false;
+
+	m_whiskerAngle = 45;
+}
+
+Agent::~Agent()
+= default;
+
+glm::vec2 Agent::GetTargetPosition() const
+{
+	return m_targetPosition;
+}
+
+glm::vec2 Agent::GetCurrentDirection() const
+{
+	return m_currentDirection;
+}
+
+float Agent::GetLOSDistance() const
+{
+	return m_LOSDistance;
+}
+
+bool Agent::HasLOS() const
+{
+	return m_hasLOS;
+}
+
+float Agent::GetCurrentHeading() const
+{
+	return m_currentHeading;
+}
+
+glm::vec4 Agent::GetLOSColour() const
+{
+	return m_LOSColour;
+}
+
+glm::vec2 Agent::GetLeftLOSEndPoint() const
+{
+	return m_leftLOSEndPoint;
+}
+
+glm::vec2 Agent::GetMiddleLOSEndPoint() const
+{
+	return m_middleLOSEndPoint;
+}
+
+glm::vec2 Agent::GetRightLOSEndPoint() const
+{
+	return m_rightLOSEndPoint;
+}
+
+glm::vec2 Agent::GetLeftLeftLOSEndPoint() const
+{
+	return m_leftleftLOSEndPoint;
+}
+
+glm::vec2 Agent::GetRightRightLOSEndPoint() const
+{
+	return m_rightrightLOSEndPoint;
+}
+
+bool* Agent::GetCollisionWhiskers()
+{
+	return m_collisionWhiskers;
+}
+
+glm::vec4 Agent::GetLineColour(const int index)
+{
+	return m_lineColour[index];
+}
+
+float Agent::GetWhiskerAngle() const
+{
+	return m_whiskerAngle;
+}
+
+void Agent::SetLeftLOSEndPoint(const glm::vec2 point)
+{
+	m_leftLOSEndPoint = point;
+}
+
+void Agent::SetMiddleLOSEndPoint(const glm::vec2 point)
+{
+	m_middleLOSEndPoint = point;
+}
+
+void Agent::SetRightLOSEndPoint(const glm::vec2 point)
+{
+	m_rightLOSEndPoint = point;
+}
+
+void Agent::SetLeftLeftLOSEndPoint(glm::vec2 point)
+{
+	m_leftleftLOSEndPoint = point;
+}
+
+void Agent::SetRightRightLOSEndPoint(glm::vec2 point)
+{
+	m_rightrightLOSEndPoint = point;
+}
+
+void Agent::SetLineColour(const int index, const glm::vec4 colour)
+{
+	m_lineColour[index] = colour;
+}
+
+void Agent::SetWhiskerAngle(const float angle)
+{
+	m_whiskerAngle = angle;
+}
+
+void Agent::UpdateWhiskers(const float angle)
+{
+	m_whiskerAngle = angle;
+
+	// Center / Middle whisker
+	SetMiddleLOSEndPoint(GetTransform()->position + GetCurrentDirection() * GetLOSDistance());
+
+	// Left Whisker
+	float x = sin((GetCurrentHeading() - m_whiskerAngle + 90.0f) * Util::Deg2Rad);
+	float y = cos((GetCurrentHeading() - m_whiskerAngle + 90.0f) * Util::Deg2Rad);
+
+	SetLeftLOSEndPoint(GetTransform()->position + glm::vec2(x, -y) * GetLOSDistance() * 0.75f);
+
+	// Left Left Whisker
+	x = sin((GetCurrentHeading() - (m_whiskerAngle + 45.0f) + 90.0f) * Util::Deg2Rad);
+	y = cos((GetCurrentHeading() - (m_whiskerAngle + 45.0f) + 90.0f) * Util::Deg2Rad);
+
+	SetLeftLeftLOSEndPoint(GetTransform()->position + glm::vec2(x, -y) * GetLOSDistance() * 0.75f);
+
+	// Right Whisker
+	x = sin((GetCurrentHeading() + m_whiskerAngle + 90.0f) * Util::Deg2Rad);
+	y = cos((GetCurrentHeading() + m_whiskerAngle + 90.0f) * Util::Deg2Rad);
+
+	SetRightLOSEndPoint(GetTransform()->position + glm::vec2(x, -y) * GetLOSDistance() * 0.75f);
+
+	// Right Right Whisker
+	x = sin((GetCurrentHeading() + (m_whiskerAngle + 45.0f) + 90.0f) * Util::Deg2Rad);
+	y = cos((GetCurrentHeading() + (m_whiskerAngle + 45.0f) + 90.0f) * Util::Deg2Rad);
+
+	SetRightRightLOSEndPoint(GetTransform()->position + glm::vec2(x, -y) * GetLOSDistance() * 0.75f);
+}
+
+void Agent::SetTargetPosition(const glm::vec2 new_position)
+{
+	m_targetPosition = new_position;
+}
+
+void Agent::SetCurrentDirection(const glm::vec2 new_direction)
+{
+	m_currentDirection = new_direction;
+}
+
+void Agent::SetLOSDistance(const float distance)
+{
+	m_LOSDistance = distance;
+}
+
+void Agent::SetHasLOS(const bool state)
+{
+	m_hasLOS = state;
+	m_LOSColour = (m_hasLOS) ? glm::vec4(0, 1, 0, 1) : glm::vec4(1, 0, 0, 1);
+}
+
+void Agent::SetCurrentHeading(const float heading)
+{
+	m_currentHeading = heading;
+	ChangeDirection();
+}
+
+void Agent::SetLOSColour(const glm::vec4 colour)
+{
+	m_LOSColour = colour;
+}
+
+void Agent::ChangeDirection()
+{
+	const auto x = cos(m_currentHeading * Util::Deg2Rad);
+	const auto y = sin(m_currentHeading * Util::Deg2Rad);
+	m_currentDirection = glm::vec2(x, y);
+}
